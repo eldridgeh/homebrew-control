1cac176350e72a7c4ecd1a101aaf2e3967a821f8
Eldridge Henley <eldridge@qbox.io>
2019-04-04 15:45:35 -0500

    wip

--- control.rb
+++ control.rb
@@ -8,13 +8,12 @@ class Control < Formula
   sha256 "62166c2b28d5c868bae9ac96f9974bc0836662a70c5e061bab4357bee7ffd078"
   # depends_on "cmake" => :build
   depends_on "go" => :build
-  depends_on "npm" => :build
+  depends_on "npm"
   depends_on "govendor" => :recommended
 
   def install
     #mkdir_p buildpath/"src/github.com/supergiant/"
     system "cd #{buildpath}"
-    system "which npm"
     system "make", "build-ui"
 
   end
