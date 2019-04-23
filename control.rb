43b3e435a134637082fccab5c1c27d1287535978
Eldridge Henley <eldridge@qbox.io>
2019-04-23 08:17:58 -0500

    Changed formula to point to osx binary This is the first commit for the osx binary, this wont work, just wanted to get it on the githubs.

--- control.rb
+++ control.rb
@@ -1,35 +1,34 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+#                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Control < Formula
   desc "Control manages the lifecycle of clusters on your infrastructure and allows deployment of applications via HELM. Its deployment and configuration workflows will help you to get up and running with Kubernetes faster."
   homepage "https://supergiant.io/toolkit/"
-  url "https://github.com/supergiant/control/archive/v2.0.0.tar.gz"
-  sha256 "62166c2b28d5c868bae9ac96f9974bc0836662a70c5e061bab4357bee7ffd078"
+  url "https://github.com/supergiant/control/releases/download/v2.1.0/supergiant_v2.1.0_Darwin_x86_64.tar.gz"
+  sha256 "9a28ed2241f6a3cde5dc5605b076ab9151a94bc914499f5f0ac135acae75b613"
   # depends_on "cmake" => :build
-  depends_on "go" => :build
-  depends_on "npm"
-  #depends_on "statik"
-  depends_on "govendor" => :recommended
-
-
 
   def install
-    #mkdir_p buildpath/"src/github.com/supergiant/"
-    ENV["GOPATH"] = buildpath
-    #system "cd #{buildpath}"
-    bin_path = buildpath/"src/github.com/supergiant/control1"
-    bin_path.install Dir["*"]
-    cd bin_path do
-      system "/usr/local/Cellar/node/11.13.0/libexec/bin/npm install --prefix ./cmd/ui/assets"
-      system "/usr/local/Cellar/node/11.13.0/libexec/bin/npm run build:prod --prefix ./cmd/ui/assets"
-      system "/usr/local/opt/go/bin/go get github.com/rakyll/statik"
-      system "#{buildpath}/.brew_home/go/bin/statik -src=./cmd/ui/assets/dist"
-      #system "GOOS=darwin CGO_ENABLED=0 GOARCH=amd64 /usr/local/opt/go/bin/go build -o dist/controlplane-osx -a -installsuffix cgo -ldflags='-extldflags \"-static\" -w -s -X main.version=${VERSION}' ./cmd/controlplane"
-    end
-
-
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
   end
 
-
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test control`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
 end
