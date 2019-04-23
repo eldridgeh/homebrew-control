54584379c11fe951084b63c4206f1201444f2852
Eldridge Henley <eldridge@qbox.io>
2019-04-23 08:21:52 -0500

    starting binary

--- control.rb
+++ control.rb
@@ -11,12 +11,7 @@ class Control < Formula
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "./supergiant
   end
 
   test do
