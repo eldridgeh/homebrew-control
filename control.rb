6d9cae5341a97ac2e11ad5a3e951d4d769409a79
Eldridge Henley <eldridge@qbox.io>
2019-04-23 14:11:53 -0500

    testing move

--- control.rb
+++ control.rb
@@ -3,18 +3,23 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Control < Formula
   desc "Control manages the lifecycle of clusters on your infrastructure and allows deployment of applications via HELM. Its deployment and configuration workflows will help you to get up and running with Kubernetes faster."
-  homepage "https://supergiant.io/toolkit/"
-  url "https://github.com/supergiant/control/releases/download/v2.1.0/supergiant_v2.1.0_Darwin_x86_64.tar.gz"
-  sha256 "9a28ed2241f6a3cde5dc5605b076ab9151a94bc914499f5f0ac135acae75b613"
+  homepage "https://supergiant.io/"
+  version "2.1.0"
+  url "https://github.com/supergiant/supergiant/archive/v2.1.0.tar.gz"
+  sha256 "78ee9dad415b21d6bab1182f9af84d679a3394e34e295efccaa35af2b46a1fb3"
   # depends_on "cmake" => :build
   head "https://github.com/supergiant/control.git"
 
+  depends_on "go" => :build
+  depends_on "govendor" => :recommended
+
+
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    #bin.install 'supergiant'
-    mkdir_p buildpath/"/etc/supergiant/templates/"
+    mkdir_p buildpath/"src/github.com/control"
+    system "mkdir -p /etc/supergiant/templates"
     system "cp templates/* /etc/supergiant/templates/"
+    system "GOOS=darwin CGO_ENABLED=0 GOARCH=amd64 go build -o dist/controlplane-osx -a -installsuffix cgo -ldflags=\'-extldflags \"-static\" -w -s -X main.version=v2.1.0\' ./cmd/controlplane"
+    system "cp dist/controlplane-osx /bin/control"
   end
 
   test do
@@ -22,15 +27,11 @@ class Control < Formula
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test control`. Options passed
+    # software. Run the test with `brew test supergiant`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
-  end
-
-  def uninstall
-    system "rm {HOMEBREW_PREFIX}/bin/supergiant"
   end
 end
